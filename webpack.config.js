9cd31d7adacfad5868148a8a3a1101c82e69b898
Prabhu <prabhu.rk90@gmail.com>
2019-09-25 15:27:28 +0530

    Initial commit.Added test webpack config file.

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require("path");
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "hello.js",
+    path: path.resolve(__dirname, "dist")
+  }
+};
