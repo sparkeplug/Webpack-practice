a35a0d8785fef31b0a67332bd7930999af94c10e
Prabhu <prabhu.rk90@gmail.com>
2019-09-26 09:50:44 +0530

    Added HTML minification. Project complete.

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,9 @@ const path = require("path");
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -9,5 +12,29 @@ module.exports = merge(common, {
     filename: "[name].[contentHash].bundle.js",
     path: path.resolve(__dirname, "dist")
   },
-  plugins: [new CleanWebpackPlugin()]
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contentHash].css"
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+      }
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      }
+    ]
+  }
 });
