19d9daf9b845c6af1034df81b6d745e564ab3ead
Prabhu <prabhu.rk90@gmail.com>
2019-09-26 07:58:34 +0530

    Added multiple entrypoints

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,10 @@
 const path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    main: "./src/index.js",
+    vendor: "./src/vendor.js"
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html"
